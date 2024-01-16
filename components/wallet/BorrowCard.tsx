1c068d3147a0d2536dac6d6797efa7df8f7e6be0
Fedev <federicoloterstein@gmail.com>
2024-01-15 21:15:29 -0300

    refactor BorrowCard  Ui

--- components/wallet/BorrowCard.tsx
+++ components/wallet/BorrowCard.tsx
@@ -1,16 +1,36 @@
-import { HStack, Stack, Text } from '@chakra-ui/react'
-import React from 'react'
-import { SavingsIcon } from '../icons/SavingsIcons'
-import Link from 'next/link'
+import { HStack, Stack, Text } from "@chakra-ui/react";
+import React from "react";
+import { SavingsIcon } from "../icons/SavingsIcons";
+import Link from "next/link";
 
 export const BorrowCard = () => {
   return (
-  <HStack>
-    <SavingsIcon />
-    <Stack>
-      <Text>Enjoy borrowing rate discounts on GHO APY 4.49-6.42%</Text>
-      <Text as={Link} href={'/borrow'}>Invest Now</Text>
-    </Stack>
-  </HStack>
-  )
-}
+    <HStack
+      minH={"100px"}
+      borderRadius={"6px"}
+      background={"white"}
+      shadow={"0px 4px 4px 0px rgba(0, 0, 0, 0.25)"}
+      p={3}
+      justify={'space-between'}
+      mt={5}
+    >
+      <SavingsIcon />
+      <Stack>
+        <Text maxW={'200px'} fontSize={'14px'} fontWeight={600}>Enjoy borrowing rate discounts on GHO APY 4.49-6.42%</Text>
+        <Text
+          as={Link}
+          href={"/borrow"}
+          bgClip="text"
+          bgGradient={
+            "linear-gradient(93deg, rgba(120, 40, 200, 0.27) 0.58%, #006FEE 99.87%)"
+          }
+          fontSize={"12px"}
+          fontWeight={700}
+          textAlign={'end'}
+        >
+          Invest Now
+        </Text>
+      </Stack>
+    </HStack>
+  );
+};
--- components/wallet/Header.tsx
+++ components/wallet/Header.tsx
@@ -1,13 +1,25 @@
-import { Avatar, HStack, IconButton } from "@chakra-ui/react";
+import { Box, HStack } from "@chakra-ui/react";
 import Image from "next/image";
 import React from "react";
+import { ArgIcon } from "../icons/ArgIcon";
+import { ScanIcon } from "../icons/ScanIcon";
 
 export const Header = () => {
   return (
-    <HStack justify={'space-between'} >
-      <Avatar />
+    <HStack justify={"space-between"} pb={6}>
+      <Box
+        background={
+          " linear-gradient(93deg, rgba(120, 40, 200, 0.27) 0.58%, #006FEE 99.87%)"
+        }
+        borderRadius={"12px"}
+        w={"40px"}
+        h={"40px"}
+      />
       <Image src={"/logos/GHOpay.png"} alt="GHO Logo" width={158} height={37} />
-      <IconButton aria-label={""} />
+      <HStack>
+        <ScanIcon />
+        <ArgIcon />
+      </HStack>
     </HStack>
   );
 };
